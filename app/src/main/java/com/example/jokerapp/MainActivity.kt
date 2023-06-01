b5301cc814c3263847d8a8675e9c1c3c6d11ae63
Igor Leal <igorleal96@gmail.com>
2023-06-01 19:06:36 -0300

    splashscreen adicionada

--- app/src/main/java/com/example/jokerapp/MainActivity.kt
+++ app/src/main/java/com/example/jokerapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.jokerapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.widget.Toolbar
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
@@ -17,6 +18,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
